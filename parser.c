9e93c2ab431021e06233a98e4726888cfea7836a
tam-dcreator <tam@THE-7TH>
2023-07-18 19:23:54 +0100

    Took out the parse_args function from the shell.c file and put it into a seperate file for portablity

---
+++ parser.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+/**
+ * parse_args - Parse the input line into command arguments.
+ * @line: Input line.
+ * @args: Array to store the command arguments.
+ */
+void parse_args(char *line, char **args)
+{
+    int i = 0;
+    char *token;
+    const char *delimiters = " \t\r\n\a";
+
+    token = strtok(line, delimiters);
+    while (token != NULL && i < MAX_ARGS - 1)
+    {
+        args[i] = token;
+        i++;
+        token = strtok(NULL, delimiters);
+    }
+    args[i] = NULL;
+}
