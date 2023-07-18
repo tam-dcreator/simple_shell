5b27801c79002f2f8f70aeeb91524b8c51d8d57a
tam-dcreator <tam@THE-7TH>
2023-07-18 19:25:38 +0100

    Made the function betty compliant

--- parser.c
+++ parser.c
@@ -6,16 +6,16 @@
  */
 void parse_args(char *line, char **args)
 {
-    int i = 0;
-    char *token;
-    const char *delimiters = " \t\r\n\a";
+	int i = 0;
+	char *token;
+	const char *delimiters = " \t\r\n\a";
 
-    token = strtok(line, delimiters);
-    while (token != NULL && i < MAX_ARGS - 1)
-    {
-        args[i] = token;
-        i++;
-        token = strtok(NULL, delimiters);
-    }
-    args[i] = NULL;
+	token = strtok(line, delimiters);
+	while (token != NULL && i < MAX_ARGS - 1)
+	{
+		args[i] = token;
+		i++;
+		token = strtok(NULL, delimiters);
+	}
+	args[i] = NULL;
 }
