6416fbf58fd69e5b3417d3843d98b2c88c78e002
tam-dcreator <tam@THE-7TH>
2023-07-25 12:33:18 +0100

    Added main function to call functions

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+/**
+*main - Entry Function.
+*@ac: Argument count
+*@av: Argument vector
+*@envp: Array of environment strings
+*
+*Return: Always 0 on success
+*/
+int  main(___attribute___((unused)) int ac,
+	  ___attribute___((unused)) char **av, char **envp)
+{
+
+	looper();
+
+	return (EXIT_SUCCESS);
+}
